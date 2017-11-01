6c356ef2da93d34ea8ca8c1e1d436c436244a7c4
unknown <wsantos@DUB-WSANTOS.aeroflex.corp>
2017-11-01 13:16:43 +0000

    Add get todos by id

--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
+const {
+  ObjectId
+} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
+var {
+  mongoose
+} = require('./db/mongoose');
 
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+  Todo
+} = require('./models/todo');
+var {
+  User
+} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) =>{
+app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
@@ -22,14 +32,34 @@ app.post('/todos', (req, res) =>{
   });
 });
 
-app.get('/todos', (req, res) =>{
-  Todo.find().then((todos) =>{
-    res.send({todos});
-  }, (e) =>{
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    });
+  }, (e) => {
     res.status(400).send(e);
   })
 });
 
+// GET /todos/12134
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send(e);
+  }).catch((err) => {
+    res.status(400).send()
+  });
+});
 
 app.post('/users', (req, res) => {
   var user = new User({
@@ -40,13 +70,16 @@ app.post('/users', (req, res) => {
 
   user.save().then((doc) => {
     res.send(doc);
-  }, (e) =>{
+  }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.listen(3000, ()=>{
-  console.log('Started on port 3000') ;
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
 
-module.exports = {app};
+module.exports = {
+  app
+};
