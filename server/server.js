d11bbfb98308dc1b5311eb191f211f3b0a57cd1e
Wagner Santos <wagner.santos@cobham.com>
2017-11-02 13:12:07 +0000

    Add PATCH todo services

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+const _ = require('lodash')
+
 const {
   ObjectId
 } = require('mongodb');
 
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {
   mongoose
@@ -92,6 +94,28 @@ app.delete('/todos/:id', (req, res) =>{
   });
 });
 
+app.patch('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    if (!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
